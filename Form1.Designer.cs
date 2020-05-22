1288fdc7a7d73bdb49eb38947dcdcc5d55fae722
WurgerZax <55925706+WurgerZax@users.noreply.github.com>
2020-05-22 22:38:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ArraysOfObjectsNet
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.X1TextBox = new System.Windows.Forms.TextBox();
+            this.x1Label = new System.Windows.Forms.Label();
+            this.Y1Label = new System.Windows.Forms.Label();
+            this.Y1TextBox = new System.Windows.Forms.TextBox();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.poitsNumberTextBox = new System.Windows.Forms.TextBox();
+            this.InitBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.Task1Btn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(912, 357);
+            this.ExitBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(100, 28);
+            this.ExitBtn.TabIndex = 0;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // X1TextBox
+            // 
+            this.X1TextBox.Enabled = false;
+            this.X1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X1TextBox.Location = new System.Drawing.Point(301, 107);
+            this.X1TextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.X1TextBox.Name = "X1TextBox";
+            this.X1TextBox.Size = new System.Drawing.Size(65, 34);
+            this.X1TextBox.TabIndex = 1;
+            this.X1TextBox.Text = "2";
+            // 
+            // x1Label
+            // 
+            this.x1Label.AutoSize = true;
+            this.x1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x1Label.Location = new System.Drawing.Point(248, 114);
+            this.x1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.x1Label.Name = "x1Label";
+            this.x1Label.Size = new System.Drawing.Size(43, 29);
+            this.x1Label.TabIndex = 2;
+            this.x1Label.Text = "X1";
+            // 
+            // Y1Label
+            // 
+            this.Y1Label.AutoSize = true;
+            this.Y1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Y1Label.Location = new System.Drawing.Point(393, 114);
+            this.Y1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Y1Label.Name = "Y1Label";
+            this.Y1Label.Size = new System.Drawing.Size(42, 29);
+            this.Y1Label.TabIndex = 4;
+            this.Y1Label.Text = "Y1";
+            // 
+            // Y1TextBox
+            // 
+            this.Y1TextBox.Enabled = false;
+            this.Y1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Y1TextBox.Location = new System.Drawing.Point(446, 107);
+            this.Y1TextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.Y1TextBox.Name = "Y1TextBox";
+            this.Y1TextBox.Size = new System.Drawing.Size(65, 34);
+            this.Y1TextBox.TabIndex = 3;
+            this.Y1TextBox.Text = "3";
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Enabled = false;
+            this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveBtn.Location = new System.Drawing.Point(556, 98);
+            this.SaveBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(100, 50);
+            this.SaveBtn.TabIndex = 5;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(46, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Points number";
+            // 
+            // poitsNumberTextBox
+            // 
+            this.poitsNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.poitsNumberTextBox.Location = new System.Drawing.Point(272, 22);
+            this.poitsNumberTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.poitsNumberTextBox.Name = "poitsNumberTextBox";
+            this.poitsNumberTextBox.Size = new System.Drawing.Size(65, 34);
+            this.poitsNumberTextBox.TabIndex = 6;
+            this.poitsNumberTextBox.Text = "2";
+            // 
+            // InitBtn
+            // 
+            this.InitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InitBtn.Location = new System.Drawing.Point(556, 14);
+            this.InitBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.InitBtn.Name = "InitBtn";
+            this.InitBtn.Size = new System.Drawing.Size(100, 50);
+            this.InitBtn.TabIndex = 8;
+            this.InitBtn.Text = "Init";
+            this.InitBtn.UseVisualStyleBackColor = true;
+            this.InitBtn.Click += new System.EventHandler(this.InitBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(679, 198);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 178);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(619, 237);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameLabel.Location = new System.Drawing.Point(46, 114);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(78, 29);
+            this.NameLabel.TabIndex = 12;
+            this.NameLabel.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Enabled = false;
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameTextBox.Location = new System.Drawing.Point(142, 107);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(65, 34);
+            this.nameTextBox.TabIndex = 11;
+            this.nameTextBox.Text = "P1";
+            // 
+            // Task1Btn
+            // 
+            this.Task1Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Task1Btn.Location = new System.Drawing.Point(912, 22);
+            this.Task1Btn.Margin = new System.Windows.Forms.Padding(4);
+            this.Task1Btn.Name = "Task1Btn";
+            this.Task1Btn.Size = new System.Drawing.Size(100, 50);
+            this.Task1Btn.TabIndex = 13;
+            this.Task1Btn.Text = "Task1";
+            this.Task1Btn.UseVisualStyleBackColor = true;
+            this.Task1Btn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 520);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 55);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(540, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 55);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 722);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Task1Btn);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InitBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.poitsNumberTextBox);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.Y1Label);
+            this.Controls.Add(this.Y1TextBox);
+            this.Controls.Add(this.x1Label);
+            this.Controls.Add(this.X1TextBox);
+            this.Controls.Add(this.ExitBtn);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.TextBox X1TextBox;
+        private System.Windows.Forms.Label x1Label;
+        private System.Windows.Forms.Label Y1Label;
+        private System.Windows.Forms.TextBox Y1TextBox;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox poitsNumberTextBox;
+        private System.Windows.Forms.Button InitBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button Task1Btn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
